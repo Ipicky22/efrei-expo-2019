a146bbc4a5a8cc9ab3c4427770e481be20ef3e4e
Adrien <adrien.masson@hotmail.fr>
2019-09-10 08:24:46 +0200

    Design ListProducts

--- client/src/screens/DetailProduct.js
+++ client/src/screens/DetailProduct.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, View, Button } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view'
 import { useQuery } from '@apollo/react-hooks';
 import * as queries from '../apollo/query'
+import Separator from '../components/Separator'
 
 export default DetailProduct = ({ navigation }) => {
 
@@ -13,28 +14,74 @@ export default DetailProduct = ({ navigation }) => {
         }
     });
 
-    console.log("data", data)
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <Text>Name : {navigation.getParam('name')}</Text>
-            <Text>Description : {navigation.getParam('description')}</Text>
-            <Text>Price : {navigation.getParam('price')}</Text>
-            <Text>Category : {navigation.getParam('category')}</Text>
-            {!loading && !error && (
-                <>
-                    <Text>firstname : {data.user.firstname}</Text>
-                    <Text>lastname : {data.user.lastname}</Text>
-                    <Text>email : {data.user.email}</Text>
-                    <Text>state : {data.user.state}</Text>
-                    <Text>city : {data.user.city}</Text>
-                    <Text>address : {data.user.address}</Text>
-                </>
-            )}
-            <TouchableOpacity onPress={} >
-                <Text>ADD FAVORITE</Text>
-            </TouchableOpacity>
+            <View style={{ display: "flex", flex: 1, marginTop: 8 }}>
+                <View style={{ height: 350, width: 200, backgroundColor: "red", margin: 8, alignSelf: "center" }}></View>
+                <View style={{ marginTop: 16, marginHorizontal: 8 }}>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                        <Text style={{ fontWeight: "600", marginVertical: 2 }}>Name :</Text>
+                        <Text style={{ fontWeight: "800", marginVertical: 2 }}>{navigation.getParam('name')}</Text>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                        <Text style={{ fontWeight: "600", marginVertical: 2 }}>Edition :</Text>
+                        <Text style={{ fontWeight: "700", marginVertical: 2 }}>{navigation.getParam('description')}</Text>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                        <Text style={{ fontWeight: "600", marginVertical: 2 }}>Price :</Text>
+                        <Text style={{ fontWeight: "600", marginVertical: 2 }}>{navigation.getParam('price')} Euros</Text>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                        <Text style={{ fontWeight: "600", marginVertical: 2 }}>TCG :</Text>
+                        <Text style={{ fontWeight: "400", marginVertical: 2 }}>{navigation.getParam('category')}</Text>
+                    </View>
+                </View>
+                {!loading && !error && (
+                    <>
+                        <View style={{ marginHorizontal: 8, marginVertical: 8 }}>
+                            <Separator />
+                        </View>
+                        <View style={{ marginHorizontal: 8 }}>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                                <Text style={{ fontWeight: "600", marginVertical: 2 }}>Firstname :</Text>
+                                <Text style={{ fontWeight: "800", marginVertical: 2 }}>{data.user.firstname}</Text>
+                            </View>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                                <Text style={{ fontWeight: "600", marginVertical: 2 }}>Lastname :</Text>
+                                <Text style={{ fontWeight: "600", marginVertical: 2 }}>{data.user.lastname}</Text>
+                            </View>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                                <Text style={{ fontWeight: "600", marginVertical: 2 }}>Email :</Text>
+                                <Text style={{ fontWeight: "400", marginVertical: 2 }}>{data.user.email}</Text>
+                            </View>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                                <Text style={{ fontWeight: "600", marginVertical: 2 }}>State :</Text>
+                                <Text style={{ fontWeight: "200", marginVertical: 2 }}>{data.user.state}</Text>
+                            </View>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                                <Text style={{ fontWeight: "600", marginVertical: 2 }}>City :</Text>
+                                <Text style={{ fontWeight: "200", marginVertical: 2 }}>{data.user.city}</Text>
+                            </View>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                                <Text style={{ fontWeight: "600", marginVertical: 2 }}>Address :</Text>
+                                <Text style={{ fontWeight: "200", marginVertical: 2 }}>{data.user.address}</Text>
+                            </View>
+                        </View>
+                    </>
+                )}
+                <View style={{ marginHorizontal: 8, marginVertical: 8 }}>
+                    <Separator />
+                </View>
+                <View style={{ marginHorizontal: 8 }}>
+                    <Text style={{ fontWeight: "600", marginVertical: 2 }}>Currently 10 people are interested in this card.</Text>
+                </View>
+                <View>
+                    <Button title="Follow" />
+                    <Button title="Share" />
+                </View>
+            </View>
         </SafeAreaView>
-    );
+    )
 }
 DetailProduct.navigationOptions = {
     title: 'Detail Product'
--- client/src/screens/ListProducts.js
+++ client/src/screens/ListProducts.js
@@ -3,6 +3,7 @@ import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view'
 import { useQuery } from '@apollo/react-hooks';
 import * as queries from '../apollo/query'
+import Separator from '../components/Separator'
 
 export default ListProducts = ({ navigation }) => {
 
@@ -16,13 +17,21 @@ export default ListProducts = ({ navigation }) => {
                     keyExtractor={({ id }) => id}
                     renderItem={({ item: { name, description, price, picture, category, id, idUser } }) => (
                         <TouchableOpacity onPress={() => navigation.navigate('DetailProduct', { name, description, price, picture, category, id, idUser })} >
-                            <View>
-                                <Text>{name}</Text>
-                                <Text>{description}</Text>
-                                <Text>{price}</Text>
-                                <Text>{category}</Text>
-                                {/* <Image source={require("../../assets/" + picture )} style={{height: 50, width: 50}} /> */}
-                            </View>
+                            <>
+                                <View style={{ display: "flex", flex: 1, flexDirection: "row", alignItems: "center" }}>
+                                    <View style={{ height: 100, width: 75, backgroundColor: "red", margin: 8 }}></View>
+                                    <View style={{ marginLeft: 8 }}>
+                                        <Text style={{ fontWeight: "800", marginVertical: 2 }}>{name}</Text>
+                                        <Text style={{ fontWeight: "600", marginVertical: 2 }}>{description}</Text>
+                                        <Text style={{ fontWeight: "400", marginVertical: 2 }}>Price : {price} Euros</Text>
+                                        <Text style={{ fontWeight: "200", marginVertical: 2 }}>{category}</Text>
+                                        {/* <Image source={require("../../assets/" + picture )} style={{height: 50, width: 50}} /> */}
+                                    </View>
+                                </View>
+                                <View style={{ marginHorizontal: 8, marginVertical: 4 }}>
+                                    <Separator />
+                                </View>
+                            </>
                         </TouchableOpacity>
                     )}
                 />
